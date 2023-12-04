59fe4f0d8249ba6b92a69e18f100aa81fc4208d5
Lalundra <145946674+Lalundra@users.noreply.github.com>
2023-12-04 10:31:58 +0800

    KULIAH JAVA PERTAMA

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //membuat sebuah variabel dan tipe data//
+
+        String nama;
+        String nim;
+        String prodi;
+        String alamat;
+
+        nama = "Lalu Andra Khairil Ramadhan";
+        nim = "23241009";
+        prodi = "PENDIDKAN TEKNOLOGI INFORMASI";
+        alamat ="Muncan";
+        
+
+        //mengeprint
+
+        System.out.println("Nama Lengkap :" + nama);
+        System.out.println("Nim Mahasiswa :"+ nim);
+        System.out.println("Prodi :"+ prodi);
+        System.out.println("Alamat :"+ alamat);
+        }
+}
